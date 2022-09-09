cb88e5e346ebaaccd674aac8dd14d99846b01ee1
jadenrowlings <sarpeyjaden@gmail.com>
2022-09-08 23:01:51 -0700

    Task 1 to 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <studio.h>
+
+/** 
+* main - print thes string in the function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/                                                                                      
+
+int main(void)                                                                          
+
+{                                                                                       
+
+        puts("\"Programming is like building a multilingual puzzle");                   
+
+        return (0);
+}	
+
