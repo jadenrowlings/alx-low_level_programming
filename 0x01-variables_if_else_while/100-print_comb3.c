9473c4b4b1d23a2cad5246fdfae98530fd6c6b18
jadenrowlings <sarpeyjaden@gmail.com>
2022-09-09 22:11:29 -0700

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,7 +2,6 @@
 /**
  * main - printing two digit numbers from 01-89 with commas and space between them
  * Description: using the main function
- * this program prints "01, 02, 03, 04, 05, 06, 07, 08, 09, 12, 13, 14, 15, 16, 17, 18, 19, 23, 24, 25, 26, 27, 28, 29, 34, 35, 36, 37, 38, 39, 45, 46, 47, 48, 49, 56, 57, 58, 59, 67, 68, 69, 78, 79, 89"
  * Return: 0
  */
 int main(void)
